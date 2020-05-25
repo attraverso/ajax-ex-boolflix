e69808eb0d62ec33ef14e7a82c36fb7bdf16e389
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-05-25 17:52:45 +0200

    get results for both movies and tv shows

--- main.js
+++ main.js
@@ -2,23 +2,39 @@ $(document).ready(function() {
 
   /*compile template*/
   var listTemplateSource = $('#list-template').html();
-  var starsTemplateSource = $('#stars-template').html();
   var listTemplate = Handlebars.compile(listTemplateSource);
-  var starsTemplate = Handlebars.compile(starsTemplateSource);
 
-  /*on click on search button*/
+  /*when Enter is pressed*/
   $('.searchbar').keypress(function(event) {
     if (event.which == 13) {
       search();
     }
-  });/*keypress end*/
+  });
 
   /*on click on search button*/
   $('.header-right .fa-search').click(function () {
     search();
-  });/*click end*/
+  });
 
-/* * FUNCTIONS * */
+/* * FUNCTIONS - alphabetical order * */
+
+/*CALL-AJAX - */
+function callAjax(searchString, url) {
+  $.ajax({
+    'url': url,
+    'method': 'get',
+    'data': {
+      'api_key': 'e99307154c6dfb0b4750f6603256716d',
+      'query': searchString,
+    },
+    'success': function(data) {
+      handleData(data.results);
+    },
+    'error': function() {
+      alert('Seems like we had a problem retrieving your search.');
+    }
+  })/*ajax end*/
+}
 
 /*HANDLE-DATA - cycle every object in the result of your query*/
 function handleData(resultsArray) {
@@ -30,28 +46,28 @@ function handleData(resultsArray) {
   }
 };
 
+/*PRINT-FLAG-IF-AVAILABLE - if you have a flag icon for the requestedlanguage, show that. Otherwise show language code.*/
 function printFlagIfAvaliable(forItem) {
-  var availableLanguages = ['de', 'en', 'es', 'fr', 'it'];
+  var availableLanguages = ['ar', 'de', 'en', 'es', 'fr', 'it', 'zh'];
   if (availableLanguages.includes(forItem.original_language.toString())) {
-    console.log('snafu is present');
     $('.movie-card:last-child li[data-info-type="language"]').remove();
   } else {
-    console.log("snafu isn't here");
     $('.movie-card:last-child li[data-info-type="lang-flag"]').remove();
   }
-}
+};
 
 /*PRINT-MOVIE-DETAILS - grab info on a singe item and print it in the template*/
 function printMovieDetails(item) {
   var context = {
-    'title': item.title,
-    'originalTitle': item.original_title,
+    'title': item.title || item.name,
+    'originalTitle': item.original_title || item.original_name,
     'language': item.original_language,
-    'flag': item.original_language,
   }
-  /*** DELETE - var created because I needed the log ***/
+  /*** DELETE - vars created because I needed the log ***/
   var title = item.title;
   console.log(title);
+  var name = item.name;
+  console.log(name);/*end D*/
 
   var html = listTemplate(context);
   $('#results-display').append(html);
@@ -60,15 +76,13 @@ function printMovieDetails(item) {
 /*PRINT-STARS - get average vote, fit to scale of 5, print 5 empty stars and fill them out according to average vote*/
 function printStars(voteAverage) {
   var starsNr = Math.round(voteAverage / 2);
-  console.log(starsNr);
   var stars = $('.movie-card:last-child li[data-info-type="vote"] i').slice(0, starsNr);
-  console.log(stars);
-  for (var a = 0; a < stars.length; a++) {
-    $('.movie-card:last-child li[data-info-type="vote"] i:nth-child('+(a + 1)+')').removeClass('far').addClass('fas');
+  for (var i = 0; i < stars.length; i++) {
+    $('.movie-card:last-child li[data-info-type="vote"] i:nth-child('+(i + 1)+')').removeClass('far').addClass('fas');
   }
 }
 
-/*SEARCH - */
+/*SEARCH - start ajax call(s) based on the user's search term*/
 function search() {
   /*grab user search*/
   var userSearch = $('.searchbar').val().trim();
@@ -78,20 +92,12 @@ function search() {
     $('.searchbar').val('');
     /*remove any cards already in page*/
     $('#results-display .movie-card').remove();
-    $.ajax({
-      'url': 'https://api.themoviedb.org/3/search/movie',
-      'method': 'get',
-      'data': {
-        'api_key': 'e99307154c6dfb0b4750f6603256716d',
-        'query': userSearch,
-      },
-      'success': function(data) {
-        handleData(data.results);
-      },
-      'error': function() {
-        alert('Seems like we had a problem retrieving your search.');
-      }
-    })/*ajax end*/
+
+    var moviesUrl = 'https://api.themoviedb.org/3/search/movie';
+    var seriesUrl = 'https://api.themoviedb.org/3/search/tv';
+
+    callAjax(userSearch, moviesUrl);
+    callAjax(userSearch, seriesUrl);
   }/*if end*/
 };
 
